5a8318b3b860a608dd194c1d8f359d8ccdd33ed1
Anthony Valle <valleganthony@gmail.com>
2019-05-23 15:57:27 -0400

    working project

--- src/main/java/com/example/springboot_401/SecurityConfiguration.java
+++ src/main/java/com/example/springboot_401/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -35,6 +36,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authorities("USER");
 
     }
+
+
 
     }
 
